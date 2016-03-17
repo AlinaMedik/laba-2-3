14b49d0cabc6b254ead7726785455794ab0f6e15
Сладкомедова Алина Андреевна <ri58s16@lwp06.csc.local>
2016-03-17 14:56:14 +0600

    разработка

---
+++ kv.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, x, d;
+	
+	printf ("wedite koeffizient 1\n");
+	scanf ("%d", &a);
+	
+	printf ("wedite koeffizient 2\n");
+    scanf ("%d", &b);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
