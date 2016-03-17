1216ba1dee5e7e924fc3680c5a684e8c23da646a
Сладкомедова Алина Андреевна <ri58s16@lwp06.csc.local>
2016-03-17 15:06:58 +0600

    разработка полностью

--- kv.c
+++ kv.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	int a, b, x, d;
+	int a, b, c, x;
+	double d;
 	
 	printf ("wedite koeffizient 1\n");
 	scanf ("%d", &a);
@@ -10,31 +12,21 @@ int main()
 	printf ("wedite koeffizient 2\n");
     scanf ("%d", &b);
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    printf ("wedite koeffizient 3\n");
+    scanf ("%d", &c);
 
+	d=sqrt(b*b-4*a*c);
+	
+	if d>=0
+	{
+		if d>0
+		{
+			printf ("x 1=%d", (-b-d)/2*a);
+			printf ("x 2=%d", (-b+d)/2*a);
+		}
+		
+		printf ("x=%d", (-b)/2*a);
+	}
+    
+    printf ("net recheni");
 }
